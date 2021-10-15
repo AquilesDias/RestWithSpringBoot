5a347cfa9e177ec523650553cdf6b8ce6e02296e
AquilesDias <kilesax@gmail.com>
2021-10-15 15:44:40 -0300

    Implementing dto pattern

---
+++ 5 - Implementing VO pattern/src/main/java/br/com/development/spring/restwithspringboot/controller/PersonController.java
@@ -0,0 +1,42 @@
+package br.com.development.spring.restwithspringboot.controller;
+
+import br.com.development.spring.restwithspringboot.DTO.PersonDto;
+import br.com.development.spring.restwithspringboot.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    PersonServices personServices;
+
+    @PostMapping
+    public PersonDto person(@RequestBody PersonDto personDto){
+        return personServices.create(personDto);
+    }
+
+    @GetMapping
+    public List<PersonDto> findByAll(){
+        return personServices.findByAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDto findById(@PathVariable("id") Long id){
+        return personServices.findById(id);
+    }
+
+    @PutMapping
+    public PersonDto update(@RequestBody PersonDto personDto){
+        return personServices.update(personDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ 5 - Implementing VO pattern/src/main/java/br/com/development/spring/restwithspringboot/exception/handler/CustomizedExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.com.development.spring.restwithspringboot.exception.handler;
+
+import br.com.development.spring.restwithspringboot.exception.ExceptionResponse;
+import br.com.development.spring.restwithspringboot.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handlerExceptionResponse(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(
+                        new Date(),
+                        ex.getMessage(),
+                        request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(
+                        new Date(),
+                        ex.getMessage(),
+                        request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
